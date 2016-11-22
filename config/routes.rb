d79fdf92decaf0d0afd4ff1ed595d84a315793e3
Mike Barile <mikebarile13@gmail.com>
2016-11-22 11:16:27 -0800

    Added article api views for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#show'
-  resources :static_pages, only: [:show]
-
   namespace :api, defaults: {format: :json} do
     resources :articles
   end
