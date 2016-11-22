fff3403e152ffc9a0c10ed3b7b89cbc28faca7dd
Mike Barile <mikebarile13@gmail.com>
2016-11-22 11:05:19 -0800

    Implemented models for article, source, and match

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+  validates :title, :source_name, :date, :author, :image_url, :article_url,
+    :description, :keywords, presence: true
+
+  def matches
+    Match.where("first_article_id = #{self.id} OR second_article_id = #{self.id}")
+  end
+
+  belongs_to :source,
+  primary_key: :private_name,
+  foreign_key: :source_name,
+  class_name: :Source
 end
