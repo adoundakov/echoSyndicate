59604a35db372000bda34967faf854584575cce7
ethankong113 <kongwan113@gmail.com>
2016-11-23 20:03:36 -0800

    added redux cycle and rending articles

--- frontend/reducers/article_reducer.js
+++ frontend/reducers/article_reducer.js
@@ -1,11 +1,18 @@
+import { merge } from 'lodash';
+import { RECEIVE_ARTICLES } from '../actions/article_actions';
+
 const _nullArticles = {
-  list: {},
+  list: [],
   errors: []
 };
 
 const ArticleReducer = (state = _nullArticles, action) => {
   Object.freeze(state);
+  let newState;
   switch(action.type) {
+    case RECEIVE_ARTICLES:
+      newState = merge({}, state, {list: action.articles});
+      return newState;
     default:
       return state;
   }
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createStore } from 'redux';
 import RootReducer from '../reducers/root_reducer';
-// import MasterMiddleware from '../middlewares/master_middleware';
+import MasterMiddleware from '../middlewares/master_middleware';
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState);
+  return createStore(RootReducer, preloadedState, MasterMiddleware);
 };
 
 export default configureStore;
