595278f47dac7cfbf549b828d1bbb4ea700f2d64
ethankong113 <kongwan113@gmail.com>
2016-11-28 19:19:08 -0800

    First draft of frontend

--- app/views/api/articles/_article.json.jbuilder
+++ app/views/api/articles/_article.json.jbuilder
@@ -1,3 +1,4 @@
 json.extract! article, :id, :title, :date, :author,
               :image_url, :article_url, :description
 json.set! :source_name, article.source.public_name
+json.set! :source_logo, article.source.logo_url
--- frontend/components/article/article.jsx
+++ frontend/components/article/article.jsx
@@ -4,17 +4,24 @@ class Article extends React.Component {
   componentWillMount() {
   }
 
+  renderImage(source, img) {
+    if (source !== "USA Today") {
+      return <div className="article-img-frame"><img src={img} className="article-img" /></div>
+    }
+  }
+
   render() {
-    const { title, date, author, image_url, source_name, description, article_url, pos } = this.props;
+    let { title, date, author, image_url, source_name, description, article_url, pos, source_logo } = this.props;
     return (
       <div className={`article-box-${pos}`}>
         <div className="source-side">
-          <div className="source-img-frame">
-            <img src={image_url} className="source-img"/>
+          <div className="source-logo-frame">
+            <img src={source_logo} className="source-logo"/>
           </div>
         </div>
-        <a href={article_url} target="_blank">
-          <div className="article-side">
+        <a href={article_url} target="_blank" className="article-side">
+          <div className="article-detail">
+            { this.renderImage(source_name, image_url) }
             <div className="article-title">{title}</div>
             <div className="article-author">By {author}</div>
             <div className="article-date">{date}</div>
