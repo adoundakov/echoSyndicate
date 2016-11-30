dabe168d6ab58d798ca2d7f36d97c8c3dc4d6d63
Mike Barile <mikebarile13@gmail.com>
2016-11-29 18:06:30 -0800

    Updated header with new logo

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = (props) => {
   return (
     <header>
+      <img src="https://res.cloudinary.com/dsguwnfdw/image/upload/v1480470866/15280962_10154428901223381_591263560_n.png_qeis3e.png" className="logo-image"/>
       <h1 className="header-text">Echo Syndicate</h1>
     </header>
   );
